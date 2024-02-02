f2b43795d5a0a7a27eb4f5aa862f76c4fbc2868d
Romuald Brillout <git@brillout.com>
2024-02-02 17:38:35 +0100

    minor refactor

--- src/Logs/isTolerateError.ts
+++ src/Logs/isTolerateError.ts
@@ -9,7 +9,7 @@ type LogData = { logSource: LogSource; logText: string }
 function isTolerateError({ logSource, logText }: LogData): boolean {
   logText = stripAnsi(logText)
 
-  if (tolerateError({ logSource, logText })) {
+  if (tolerateErrorBuiltIn({ logSource, logText })) {
     return true
   }
   const config = getConfig()
@@ -19,49 +19,42 @@ function isTolerateError({ logSource, logText }: LogData): boolean {
   return false
 }
 
-function tolerateError({ logSource, logText }: LogData): boolean {
-  return isFetchExperimentalWarning() || isTerminationEPIPE() || isChromeCannotCreepyTrackWarning()
+function tolerateErrorBuiltIn({ logSource, logText }: LogData): boolean {
+  return (
+    [
+      {
+        logText: 'ExperimentalWarning: The Fetch API is an experimental feature. This feature could change at any time',
+        logSource: 'stderr',
+      },
 
-  function isFetchExperimentalWarning() {
-    return (
-      logSource === 'stderr' &&
-      logText.includes(
-        'ExperimentalWarning: The Fetch API is an experimental feature. This feature could change at any time'
-      )
-    )
-  }
+      /* Ignore:
+      ```
+      [16:00:26.335][/examples/i18n-v1][npm run dev][run()] Terminate server.
+      [16:00:26.342][/examples/i18n-v1][npm run dev][stderr] The service was stopped: write EPIPE
+      [16:00:26.342][/examples/i18n-v1][npm run dev][run()] Process termination. (Nominal, exit code: null.)
+      ``` */
+      {
+        logText: 'The service was stopped: write EPIPE',
+        logSource: 'stderr',
+      },
 
-  /* Suppress:
-  ```
-  [16:00:26.335][/examples/i18n-v1][npm run dev][run()] Terminate server.
-  [16:00:26.342][/examples/i18n-v1][npm run dev][stderr] The service was stopped: write EPIPE
-  [16:00:26.342][/examples/i18n-v1][npm run dev][run()] Process termination. (Nominal, exit code: null.)
-  ```
-  */
-  function isTerminationEPIPE() {
-    return logSource === 'stderr' && logText.includes('The service was stopped: write EPIPE')
-  }
-
-  /* Ignore Chrome logging warning when it cannot do its creepy tracking: https://stackoverflow.com/questions/69619035/error-with-permissions-policy-header-unrecognized-feature-interest-cohort/75119417#75119417
-     ```
-     [15:57:20.273][/test/abort/test-dev-server.test.ts][npm run dev:server][Browser Warning] {
-       "type": "warning",
-       "text": "Error with Permissions-Policy header: Origin trial controlled feature not enabled: 'interest-cohort'.",
-       "location": {
-         "url": "",
-         "lineNumber": 0,
-         "columnNumber": 0
-       },
-       "args": []
-     }
-     ```
-  */
-  function isChromeCannotCreepyTrackWarning() {
-    return (
-      logSource === 'Browser Warning' &&
-      logText.includes(
-        "Error with Permissions-Policy header: Origin trial controlled feature not enabled: 'interest-cohort'."
-      )
-    )
-  }
+      /* Ignore Chrome logging warning when it cannot do its creepy tracking: https://stackoverflow.com/questions/69619035/error-with-permissions-policy-header-unrecognized-feature-interest-cohort/75119417#75119417
+      ```
+      [15:57:20.273][/test/abort/test-dev-server.test.ts][npm run dev:server][Browser Warning] {
+        "type": "warning",
+        "text": "Error with Permissions-Policy header: Origin trial controlled feature not enabled: 'interest-cohort'.",
+        "location": {
+          "url": "",
+          "lineNumber": 0,
+          "columnNumber": 0
+        },
+        "args": []
+      }
+      ``` */
+      {
+        logText: 'Error with Permissions-Policy header: Origin trial controlled feature not enabled',
+        logSource: 'Browser Warning',
+      },
+    ] as LogData[]
+  ).some((l) => l.logSource === logSource && logText.includes(l.logText))
 }

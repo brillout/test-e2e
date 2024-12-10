bb84ae28077ff59223b4431c5136e9b81dd54575
Lukas Waslowski <cr7pt0gr4ph7@gmail.com>
2024-12-10 13:00:36 +0100

    feat: autoRetry can now return a value

--- src/autoRetry.ts
+++ src/autoRetry.ts
@@ -3,17 +3,16 @@ export { autoRetry }
 import { TIMEOUT_AUTORETRY } from './TIMEOUTS.js'
 import { sleep } from './utils.js'
 
-async function autoRetry(
-  test: () => void | Promise<void>,
+async function autoRetry<Result = void>(
+  test: () => Result | Promise<Result>,
   { timeout = TIMEOUT_AUTORETRY }: { timeout?: number } = {},
-): Promise<void> {
+): Promise<Result> {
   const period = 100
   const numberOfTries = timeout / period
   let i = 0
   while (true) {
     try {
-      await test()
-      return
+      return await test()
     } catch (err) {
       i = i + 1
       if (i > numberOfTries) {

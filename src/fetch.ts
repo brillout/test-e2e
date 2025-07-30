acebd7f9439292fbc7bdaf3c95ed0364cc1ef4c5
Romuald Brillout <git@brillout.com>
2025-07-30 15:12:30 +0200

    fix: use standard fetch() instead of node-fetch

--- src/fetch.ts
+++ src/fetch.ts
@@ -1,35 +1,24 @@
-export { fetch }
+export { fetchAndLogError as fetch }
 export { fetchHtml }
 
 import { getServerUrl } from './getCurrentTest.js'
-import fetch_ from 'node-fetch'
-import type { RequestInfo, RequestInit, Response } from 'node-fetch'
 import { Logs } from './Logs.js'
 
 async function fetchHtml(pathname: string) {
-  const response = await fetch(getServerUrl() + pathname)
+  const response = await fetchAndLogError(getServerUrl() + pathname)
   const html = await response.text()
   return html
 }
-async function fetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
+async function fetchAndLogError(...args: Parameters<typeof fetch>): Promise<Response> {
   try {
-    // @ts-ignore
-    //
-    // I don't know why but [transition to ESM](https://github.com/brillout/test-e2e/commit/9b0822a4f5bf787de5a150173f3eeffcfa7ebc5c) broke the default importing of the node-fetch types.
-    //
-    // The following used to work but doens't anymore:
-    //  ```js
-    //  async function fetch(...args: Parameters<typeof fetch_>) {
-    //    // ...
-    //  }
-    //  ```
-    return await fetch_(url, init)
+    return await fetch(...args)
   } catch (err) {
+    const url = args[0]
+    const init = args[1]
     Logs.add({
       logSource: 'Connection Error',
       logText: `Couldn't connect to \`${url}\`. Args: \`${JSON.stringify(init)}\`. Err: \`${
-        // @ts-ignore
-        err.message
+        (err as Error).message
       }\``,
     })
     throw new Error("Couldn't connect to server. See `Connection Error` log for more details.")
